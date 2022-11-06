13c72a38afe9b4cb384b6c36c0808260a28df2aa
Jackson Freitas <jacksonzitap.mc@gmail.com>
2022-11-05 22:50:23 -0300

    Updating files for github

---
+++ main.js
@@ -0,0 +1,35 @@
+//variáveis
+const screen2 = document.querySelector(".screen2")
+const screen1 = document.querySelector(".screen1")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+const ramdomNumber = Math.round(Math.random() * 10)
+let xAttemps = 1
+
+//Eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+
+//funcoes
+function handleTryClick(event){
+    event.preventDefault()
+    const inputNumber = document.querySelector("#inputNumber")
+
+    if(Number(inputNumber.value) == ramdomNumber) {
+        toggleScreen()
+        screen2.querySelector("h2").innerText = `acertou em ${xAttemps} tentativas!`
+    }
+
+    inputNumber.value = "" 
+    xAttemps++
+}
+
+function handleResetClick(){
+    toggleScreen()
+    xAttemps = 1
+}
+
+function toggleScreen(){
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+}
